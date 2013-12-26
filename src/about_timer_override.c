a8b8cf29b262c4d7602321b69917a390bc223868
jin <hsienjin@gmail.com>
2013-12-26 09:56:47 +0800

    A more comprehensive about_timer_override demo.

--- src/about_timer_override.c
+++ src/about_timer_override.c
@@ -7,54 +7,135 @@
 #include <ev.h>
 #include "macros.h"
 
-static struct ev_timer t1;
-static struct ev_timer t2;
-static struct ev_timer t3;
-static int n1 = 0;
-static int n2 = 0;
-static int n3 = 0;
+static int verbose = 0;
 
-static void on_t1_cancel_t2(struct ev_loop * loop,
-			struct ev_timer * timer, int revents)
+struct task
 {
-	(void)revents;
-	Assert(timer == &t1);
-	n1 += 1;
-	ev_timer_stop(loop, &t2);
+	struct ev_timer timer;
+	int rank;
+};
+
+static const double T = 0.01;
+static const int N = 10;
+static int order[N];
+static struct task tasks[N];
+static int n = 0;
+
+#define dump_order() __dump_order(__OUT__)
+static void __dump_order(int lineno)
+{
+	int i;
+	printf(__FMT__ "dump_order()\n", lineno);
+	printf(__FMT__ "  n is %d\n", __OUT__, n);
+	for (i=0; i < n; i+=1)
+		printf(__FMT__ "  order[%d] is %d\n", __OUT__, i, order[i]);
 }
 
-static void on_t2_inc_n2(struct ev_loop * loop,
-			struct ev_timer * timer, int revents)
+static void register_completition(struct task * task)
 {
-	(void)loop;
-	(void)timer;
-	(void)revents;
-	n2 += 1;
+	Assert(n < N);
+	order[n] = task->rank;
+	n += 1;
 }
 
-static void on_t3_stop(struct ev_loop * loop,
-		struct ev_timer * timer, int revents)
+static void on_timeout(struct ev_loop * loop, struct ev_timer * timer,
+		int revents)
 {
-	(void)timer;
-	(void)revents;
-	n3 += 1;
-	ev_break(loop, EVBREAK_ALL);
+	struct task * task = containerof(timer, struct task, timer);
+	(void)revents;		/* unused */
+	register_completition(task);
+	ev_timer_stop(loop, timer);
+}
+
+static void on_timeout_stop_next(struct ev_loop * loop,
+				struct ev_timer * timer, int revents)
+{
+	struct task * task = containerof(timer, struct task, timer);
+	struct task * next;
+	(void)revents;		/* unused */
+	register_completition(task);
+	Assert(task->rank+1 < N);
+	next = &tasks[task->rank+1];
+	ev_timer_stop(loop, &next->timer);
+}
+
+static void on_timeout_delay_next(struct ev_loop * loop,
+				struct ev_timer * timer, int revents)
+{
+	struct task * task = containerof(timer, struct task, timer);
+	struct task * next;
+	(void)revents;		/* unused */
+	register_completition(task);
+	Assert(task->rank+1 < N);
+	next = &tasks[task->rank+1];
+	/*
+	 * NOTE: must stop timer before it can be safely reinitialized
+	 * and started.
+	 */
+	ev_timer_stop(loop, &next->timer);
+	ev_timer_init(&next->timer, on_timeout, T*N, 0);
+	ev_timer_start(loop, &next->timer);
 }
 
 int main(void)
 {
 	struct ev_loop * loop = EV_DEFAULT;
-	ev_timer_init(&t1, on_t1_cancel_t2, .1, 0);
-	ev_timer_init(&t2, on_t2_inc_n2, .2, 0);
-	ev_timer_init(&t3, on_t3_stop, .3, 0);
+	int i;
+	/* Initialize task ranks */
+	for (i=0; i<N; ++i) {
+		tasks[i].rank = i;
+	}
 
-	ev_timer_start(loop, &t1);
-	ev_timer_start(loop, &t2);
-	ev_timer_start(loop, &t3);
-
+	/* Straight timeout order */
+	n = 0;
+	for (i=0; i<N; ++i) {
+		ev_timer_init(&tasks[i].timer, on_timeout, T*i, 0);
+		ev_timer_start(loop, &tasks[i].timer);
+	}
 	Test(0 == ev_run(loop, 0));
-	Test(n1 == 1);
-	Test(n2 == 0);
-	Test(n3 == 1);
+	Test(n == N);
+	if (verbose)
+		dump_order();
+	for (i=0; i<N; ++i) {
+		Assert(order[i] == i);
+	}
+
+	/* Tasks 0, 1, 2 are scheduled.  But task 0 would stop task
+	 * 1.  Expected order is 0, 2*/
+	n = 0;
+	for (i=0; i<3; ++i) {
+		if (i == 0)
+			ev_timer_init(&tasks[i].timer, on_timeout_stop_next,
+				T*i, 0);
+		else
+			ev_timer_init(&tasks[i].timer, on_timeout, T*i, 0);
+		ev_timer_start(loop, &tasks[i].timer);
+	}
+	Test(0 == ev_run(loop, 0));
+	if (verbose)
+		dump_order();
+	Test(n == 2);
+	Test(order[0] == 0);
+	Test(order[1] == 2);
+
+	/* Tasks 0, 1, 2 are scheduled. But task 0 would reschedule 1 to
+	 * the end.  Expected order is 0, 2, 1*/
+	n = 0;
+	for (i=0; i<3; ++i) {
+		if (i == 0)
+			ev_timer_init(&tasks[i].timer, on_timeout_delay_next,
+				T*i, 0);
+		else
+			ev_timer_init(&tasks[i].timer, on_timeout, T*i, 0);
+		ev_timer_start(loop, &tasks[i].timer);
+	}
+	Test(0 == ev_run(loop, 0));
+	if (verbose)
+		dump_order();
+	Test(n == 3);
+	Test(order[0] == 0);
+	Test(order[1] == 2);
+	Test(order[2] == 1);
+
 	exit(EXIT_SUCCESS);
 }
